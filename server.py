e88dcd629f743eca8daf49df61c4a0a9239d281f
Andrew Cring <amcring1@gmail.com>
2022-08-20 14:23:49 -0700

    Setting up the server.py file

--- server.py
+++ server.py
@@ -2,10 +2,10 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask
+from flask import Flask, render_template, request, flash,redirect, session
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import connect_to_db, db, User, Movie, Rating
 
 
 app = Flask(__name__)
@@ -22,7 +22,124 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    return "<html><body>Placeholder for the homepage.</body></html>"
+
+    return render_template("homepage.html")
+
+@app.route('/register', methods=['GET'])
+def register_form():
+    """Show form for user signup."""
+
+    return render_template("register_form.html")
+
+@app.route('/register', methods=['POST'])
+def register_process():
+    """Process registration."""
+
+    email = request.form["email"]
+    password = request.form["password"]
+    age = int(request.form["age"])
+    zipcode = request.form["zipcode"]
+
+    new_user = User(email=email, password=password, age=age, zipcode=zipcode)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    flash(f"User {email} added.")
+    return redirect("/")
+
+@app.route('/login', methods=['POST'])
+def login_process():
+    """Process login"""
+
+    email = request.form["email"]
+    password = request.form["password"]
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        flash("No such user")
+        return redirect("/login")
+
+    if user.password != password:
+        flash("Incorrect password")
+        return redirect("/login")
+
+    session["user_id"] = user.user_id
+
+    flash("Logged in")
+    return redirect(f"/users/{user.user_id}")
+
+@app.route('/logout')
+def logout():
+    """Log out"""
+
+    del session["user_id"]
+    flash("Logged Out")
+    return redirect("/")
+
+@app.route("/users")
+def user_list():
+    """Show users list"""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+@app.route("/users/<int:user_id>")
+def user_detail(user_id):
+    """Show info about user"""
+
+    user = User.query.get(user_id)
+    return render_template("user.html", user=user)
+
+@app.route("/movies")
+def movie_list():
+    """Show list of movies"""
+
+    movies = Movie.query.order_by('title').all()
+    return render_template("movie_list.html", movies=movies)
+
+@app.route("/movies/<int:movie_id>", methods=['GET'])
+def movie_detail(movie_id):
+    """Show info about movie and allow user to add/edit rating"""
+
+    movie = Movie.query.get(movie_id)
+
+    user_id = session.get("user_id")
+
+    if user_id:
+        user_rating = Rating.query.filter_by(
+            movie_id=movie_id, user_id=user_id).first()
+    else:
+        user_rating = None
+
+    return render_template("movie.htmal",
+                            movie=movie,
+                            user_rating=user_rating)
+
+@app.route("/movies/<int:movie_id>", methods=["POST"])
+def movie_detail_process(movie_id):
+    """Add/edit rating"""
+
+    score = int(request.form["score"])
+
+    user_id = session.get("user_id")
+    if not user_id:
+        raise Exception("No user logged in")
+    
+    rating = Rating.query.filter_by(user_id=user_id, movie_id=movie_id).first()
+
+    if rating:
+        rating.score = score
+        flash("Rating updated")
+    else:
+        rating = Rating(user_id=user_id, movie_id=movie_id, score=score)
+        flash("Rating added")
+        db.session.add(rating)
+
+    db.session.commit()
+
+    return redirect(f"/movies/{movie_id}")
 
 
 if __name__ == "__main__":
